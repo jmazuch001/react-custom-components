7ff47ff5e62d6c9592c9307ae67bcc5afe0e8089
John Mazuch <36649122+jmazuch001@users.noreply.github.com>
2023-01-28 13:46:13 -0500

    update stage two and three form components

--- src/components/MUI Forms/StageThree.js
+++ src/components/MUI Forms/StageThree.js
@@ -2,7 +2,23 @@ import React from 'react'
 
 export default function StageTwo() {
   return (
-    <div>StageTwo</div>
+    <FormBox >
+        <FormStageContainer>
+            <h1>
+                Step 3
+            </h1>
+            <ul>
+                <label>Input Field for Value Calculation Goes Here</label>
+                    <TextInput></TextInput>
+                <label>Returns Player Split of Sale Valuation</label>
+                    <TextInput></TextInput>
+            </ul>
+            <div>
+                <CornerButton variant="contained">Next Stage</CornerButton>
+            </div>
+        </FormStageContainer>
+        
+    </FormBox>
   )
 }
 
